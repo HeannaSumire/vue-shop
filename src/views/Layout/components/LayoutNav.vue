c5a4d8294da0dd1c727aeddc4ea2932d846fea1b
Shiro <2412548129@qq.com>
2023-09-26 10:58:25 +0800

    添加部分路由跳转

--- src/views/Layout/components/LayoutNav.vue
+++ src/views/Layout/components/LayoutNav.vue
@@ -17,7 +17,9 @@ const confirm = () => {
         <!-- 多模板渲染 区分登录状态和非登录状态 是否有token -->
         <template v-if="userStore.userInfo.token">
           <li>
-            <a href="javascript:;"><i class="iconfont icon-user"></i>Me</a>
+            <a href="javascript:;" @click="$router.push('/member')"
+              ><i class="iconfont icon-user"></i>Me</a
+            >
           </li>
           <li>
             <el-popconfirm
@@ -29,7 +31,7 @@ const confirm = () => {
               <template #reference><a href="javascript:;">退出登录</a></template>
             </el-popconfirm>
           </li>
-          <li><a href="javascript:;">我的订单</a></li>
+          <li><a href="javascript:;" @click="$router.push('/member/order')">我的订单</a></li>
           <li><a href="javascript:;" @click="$router.push('/member')">会员中心</a></li>
         </template>
         <template v-else>
